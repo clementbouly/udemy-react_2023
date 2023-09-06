d53568c0c076bb3b2a7499cea5ea622a04a79594
clementbouly <clementbouly@hotmail.fr>
2023-09-06 11:03:28 +0200

    feat(comments): add IA generated comments for custom hook

--- ReactMealsProject/src/App.jsx
+++ ReactMealsProject/src/App.jsx
@@ -4,22 +4,33 @@ import MealItemsList from "./components/meals-itemsList/mealItemsList.component"
 import MealsSummary from "./components/meals-summary/mealsSummary.component"
 import { CartContextProvider } from "./store/cart.context"
 
+// Define a custom hook named useDelayUnmount that takes a boolean value isMounted as an argument
 function useDelayUnmount(isMounted) {
-	const [shouldRender, setShouldRender] = useState(false)
+	// Declare a state variable named shouldRender and initialize it to false
+	const [shouldRender, setShouldRender] = useState(false);
 
+	// Declare an effect that runs when isMounted or shouldRender changes
 	useEffect(() => {
-		let timeoutId
+		// Declare a variable named timeoutId
+		let timeoutId;
 
+		// If isMounted is true and shouldRender is false
 		if (isMounted && !shouldRender) {
-			setShouldRender(true)
-		} else if (!isMounted && shouldRender) {
-			timeoutId = setTimeout(() => setShouldRender(false), 500)
+			// Set shouldRender to true
+			setShouldRender(true);
+		} 
+		// If isMounted is false and shouldRender is true
+		else if (!isMounted && shouldRender) {
+			// Set timeoutId to a timeout function that sets shouldRender to false after 500ms
+			timeoutId = setTimeout(() => setShouldRender(false), 500);
 		}
 
-		return () => clearTimeout(timeoutId)
-	})
+		// Return a cleanup function that clears the timeoutId
+		return () => clearTimeout(timeoutId);
+	}, [isMounted, shouldRender]);
 
-	return shouldRender
+	// Return the value of shouldRender
+	return shouldRender;
 }
 
 function App() {
