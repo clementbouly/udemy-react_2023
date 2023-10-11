344d5f997e00cbc81cf3db6ad64611bc9ee8a29e
clementbouly <clementbouly@hotmail.fr>
2023-10-11 19:04:54 +0200

    feat(lazyLoading): practice react lazy loading components

--- forms/src/App.js
+++ forms/src/App.js
@@ -1,11 +1,45 @@
-import SimpleInput from './components/SimpleInput';
+import { Suspense, lazy, useState } from "react"
+import SimpleInput from "./components/SimpleInput"
 
 function App() {
-  return (
-    <div className="app">
-      <SimpleInput />
-    </div>
-  );
+	const [showComp1, setShowComp1] = useState(false)
+	const [showComp2, setShowComp2] = useState(false)
+
+	const toggleComp1 = () => {
+		setShowComp1((prevState) => !prevState)
+	}
+
+	const toggleComp2 = () => {
+		setShowComp2((prevState) => !prevState)
+	}
+
+	const getPageContent = () => {
+    const LazyComp1 = lazy(() => import("./components/comp1"))
+    const LazyComp2 = lazy(() => import("./components/comp2"))
+		return (
+			<Suspense fallback={"loading"}>
+				{showComp1 && <LazyComp1 />}
+				{showComp2 && <LazyComp2 />}
+        {!showComp1 && !showComp2 && <h1>Nothing to see here</h1>}
+			</Suspense>
+		)
+	}
+
+	return (
+		<div className={`app ${showComp1 ? "comp1" : ""} ${showComp2 ? "comp2" : ""}`}>
+			<SimpleInput />
+    
+			<h1>Lazy Loading Practice</h1>
+			<button className={`${showComp1 ? "off" : "on"}`} onClick={toggleComp1}>
+				Show Comp1
+			</button>
+			<button className={`${showComp2 ? "off" : "on"}`} onClick={toggleComp2}>
+				Show Comp2
+			</button>
+
+			{getPageContent()}
+		</div>
+	)
 }
 
-export default App;
+export default App
---
+++ forms/src/components/comp1.js
@@ -0,0 +1,7 @@
+export default function Comp1() {
+  return (
+    <div className="comp1">
+      <h1>Comp1</h1>
+    </div>
+  );
+}
