6469091911b2d94bf4ff5f2d4789606261a50597
clementbouly <clementbouly@hotmail.fr>
2024-02-08 09:21:12 +0100

    feat(): Mise en œuvre de la fonction de délai pour la recherche et optimisation de la liste filtrée.

--- 27-Patterns/src/components/SearchableList/SearchableList.jsx
+++ 27-Patterns/src/components/SearchableList/SearchableList.jsx
@@ -1,12 +1,27 @@
 import { useState } from "react"
 
+const debounce = (fn, delay) => {
+	let timeout
+
+	const debouncedFn = (...args) => {
+		clearTimeout(timeout)
+		timeout = setTimeout(() => {
+			fn(...args)
+		}, delay)
+	}
+
+	return debouncedFn
+}
+
 export default function SearchableList({ items, itemKeyFn, children }) {
 	const [searchInput, setSearchInput] = useState("")
 	const filteredItems = items.filter((item) => item.title.toLowerCase().includes(searchInput.toLowerCase()))
 
-	const handleChange = (e) => {
+	const handleChange = debounce((e) => {
 		setSearchInput(e.target.value)
-	}
+	}, 500)
+
+	console.log("SearchableList rendered")
 
 	return (
 		<div className="searchable-list">
